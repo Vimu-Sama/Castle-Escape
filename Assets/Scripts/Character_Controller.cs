1fe2851fe30f5510d4d81c81d79553a8211581e2
Vimarsh Sharma <42619785+Vimu-Sama@users.noreply.github.com>
2022-03-26 18:07:03 +0530

    added ladder climbing mechanics

--- Assets/Scripts/Character_Controller.cs
+++ Assets/Scripts/Character_Controller.cs
@@ -9,21 +9,27 @@ public class Character_Controller : MonoBehaviour
     float h;
     [SerializeField] float  run_speed= 0f ;
     [SerializeField] float jump_speed = 0f;
+    [SerializeField] float climb_speed = 0f;
     Rigidbody2D rb;
     Vector2 v;
     bool jump;
     Animator animator;
+    BoxCollider2D col;
+    CapsuleCollider2D cap_collider;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        col = GetComponentInChildren<BoxCollider2D>();
+        cap_collider = GetComponent<CapsuleCollider2D>();
     }
 
     private void Update()
     {
         run_animation();
         Run();
+        Climbing();
     }
 
     void OnMove(InputValue input)
@@ -33,14 +39,17 @@ public class Character_Controller : MonoBehaviour
 
     void OnJump(InputValue input)
     {
-        if(input.isPressed)
+        if (!col.IsTouchingLayers(LayerMask.GetMask("platform")))
+            return;
+        if(input.isPressed )
         {
-            rb.velocity += new Vector2(0f, jump_speed* Time.deltaTime);
-            
+            rb.velocity += new Vector2(0f, jump_speed);
         }
     }
     void Run()
     {
+        if (!col.IsTouchingLayers(LayerMask.GetMask("platform")) && cap_collider.IsTouchingLayers(LayerMask.GetMask("platform")))
+            return;
         if (v.x < 0)
         {
             transform.localScale = new Vector2((-1) * Mathf.Abs(transform.localScale.x), transform.localScale.y);
@@ -58,5 +67,12 @@ public class Character_Controller : MonoBehaviour
             animator.SetBool("run", true);
         else
             animator.SetBool("run", false);
+    }
+
+    void Climbing()
+    {
+        if (!cap_collider.IsTouchingLayers(LayerMask.GetMask("climbing"))) return;
+
+        rb.velocity = new Vector2(rb.velocity.x, v.y * climb_speed * Time.deltaTime);
     }
 }
